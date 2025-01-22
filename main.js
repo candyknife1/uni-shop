bc2e434183c6c196a6e38d7ad27e2ec102253a63
Vae <2632415995@qq.com>
2025-01-22 11:12:13 +0800

    完成首页功能开发

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 import App from './App'
 
+//导入网络请求包
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+//配置全局请求路径
+$http.baseUrl= 'https://api-hmugo-web.itheima.net'
+
+$http.beforeRequest = function(options){
+  uni.showLoading({
+    title: '数据加载中'
+  })
+}
+
+//响应拦截器
+$http.afterRequest = function(options){
+  uni.hideLoading()
+}
+
+uni.$showMsg = function(title= '数据请求失败' , duration = 1500){
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
+
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
